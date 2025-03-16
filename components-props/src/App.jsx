3c12b4c1d6ddafb43868483dc8b5f0c6dae56c16
Gabriel Carvalho <gabrielrenatosc@gmail.com>
2025-03-16 11:45:21 -0300

    learn: encerra a secao 2.5 da apostila 4

--- components-props/src/App.jsx
+++ components-props/src/App.jsx
@@ -4,17 +4,74 @@ const App = () => {
     <div className="container border rounded mt-2">
       <div className="row">
         <div className="col-sm-12 col-md-6 col-xxl-3">
-          <p>Caixa 1</p>
+          <div className="card">
+            <div className="card-header text-muted">
+              22/01/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-headset fa-2x fa-shake"></i>
+
+              </div>
+              <div className="flex-grow-1 ms-3 py-2">
+                <h4 className="text-center">Headset</h4>
+                <p className="text-center">Headset bluetooth</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-sm-12 col-md-6 col-xxl-3">
-          <p>Caixa 2</p>
+          <div className="card">
+            <div className="card-header text-muted">
+              01/04/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-gamepad fa-2x fa-shake"></i>
+
+              </div>
+              <div className="flex-grow-1 ms-3 py-2">
+                <h4 className="text-center">Gamepad</h4>
+                <p className="text-center">Controle Nintendo</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-sm-12 col-md-6 col-xxl-3">
-          <p>Caixa 3</p>
+          <div className="card">
+            <div className="card-header text-muted">
+              22/04/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-hippo fa-2x fa-shake"></i>
+
+              </div>
+              <div className="flex-grow-1 ms-3 py-2">
+                <h4 className="text-center">Hipopótamo</h4>
+                <p className="text-center">Hipopótamo fêmea</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-sm-12 col-md-6 col-xxl-3">
-          <p>Caixa 4</p>
+          <div className="card">
+            <div className="card-header text-muted">
+              22/04/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-snowman fa-2x fa-shake"></i>
+
+              </div>
+              <div className="flex-grow-1 ms-3 py-2">
+                <h4 className="text-center">Boneco de neve</h4>
+                <p className="text-center">Esse boneco de neve balança sozinho</p>
+              </div>
+            </div>
+          </div>
         </div>
+        
       </div>
     </div>
 
